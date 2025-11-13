b599c6d22a04a18dccbc567a9f6994a81ad40470
AntLacerda <lacerdaneto99@hotmail.com>
2025-11-13 20:22:34 -0300

    Feat: Finalizando o menu superior
    Issue: #01

--- components/top-menu.tsx
+++ components/top-menu.tsx
@@ -3,9 +3,9 @@ export default function TopMenu() {
         <header className="w-full h-12 bg-black flex items-center justify-around gap-72 text-[#FFF6D9] font-unifraktur p-5">
             <span className="">AL</span>
             <nav className="space-x-4 flex gap-6 text-[#FFF6D9]">
-                <a href="#about" className="">About</a>
-                <a href="#projects" className="">Projects</a>
-                <a href="#contact" className="">Contact</a>
+                <a href="#about" className="transition ease-in duration-200 hover:text-gray-100">About</a>
+                <a href="#projects" className="transition ease-in duration-200 hover:text-gray-100">Projects</a>
+                <a href="#contact" className="transition ease-in duration-200 hover:text-gray-100">Contact</a>
             </nav>
             <span className="">2025</span>
         </header>
